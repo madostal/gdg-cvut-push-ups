51698eafdd40ad6a96b8e7026a462b74fd49bf5e
Milan Lempera <milanlempera@gmail.com>
2016-04-29 13:52:57 +0200

    dashboard - top x%

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -3,8 +3,7 @@
 
   var DashboardController = function(dashboardData) {
 
-    console.log(dashboardData)
-
+    this.userRating = dashboardData.rating;
     this.todayPerformance = dashboardData.today;
     this.dailyPerformance = dashboardData.month.daily;
     this.monthlyActivity = dashboardData.month.activities;
