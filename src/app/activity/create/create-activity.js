f57f7e238cb290483662aebd44d9c0f283ac75df
Milan Lempera <milanlempera@gmail.com>
2016-05-02 15:50:10 +0200

    create form TODOs

--- src/app/activity/create/create-activity.js
+++ src/app/activity/create/create-activity.js
@@ -1,20 +1,28 @@
 (function() {
   'use strict';
 
-  var CreateActivityController = function($state, Activity, ngToast) {
+  var CreateActivityController = function($state, Activity, activityTypesService, ngToast) {
+
+    activityTypesService.getActivityTypes()
+      .then(function(types) {
+        this.types = types;
+      }.bind(this));
 
     this.activity = {
       date: new Date(),
       count: 0
     };
 
-    this.save = function(activity) {
-      Activity.save(activity).$promise
-        .then(function() {
-          ngToast.success('Activity created!');
-          $state.go('app.activity');
-        });
+    this.save = function(form) {
+
+      // TODO checkValidity, save, show message and redirect
+
+      function afterSave() {
+        ngToast.success('Activity created!');
+        $state.go('app.activity');
+      }
     };
+
   };
 
   angular.module('cz.angular.pushups.activity.create',
