7904ef55bc4b4384d06b6f62a27d56264cade63a
Milan Lempera <milanlempera@gmail.com>
2016-05-02 14:38:23 +0200

    fix typo - dashboard

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -1,12 +1,22 @@
 (function() {
   'use strict';
 
+  var DashboardController = function(dashboardData) {
+
+    this.userRating = dashboardData.rating;
+    this.todayPerformance = dashboardData.today;
+    this.dailyPerformance = dashboardData.month.daily;
+    this.monthlyActivity = dashboardData.month.activities;
+
+  };
+  
   angular.module('cz.angular.pushups.dashboard', [
     'ui.router',
 
     'cz.angular.pushups.dashboard.component.activityChart',
     'cz.angular.pushups.dashboard.component.dailyChart'
   ])
+    .controller('DashboardController', DashboardController)
     .config(function($stateProvider) {
 
       $stateProvider.state('app.dashboard', {
